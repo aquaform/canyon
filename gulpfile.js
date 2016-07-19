b64a885c534e0e02738e60642c2d792fb41c81f3
aquaform <aquaform@yandex.ru>
2016-07-19 17:20:59 +0500

    Поработал в колледже

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
+var browserSync = require("browser-sync");
 
 gulp.task("less", function () {
   gulp.src("less/style.less")
     .pipe(less())
-    .pipe(gulp.dest("css"));
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.reload({stream:true}));
 
+});
+
+gulp.task("browser-sync", function () {
+  browserSync({
+    server: {
+      baseDir: ""
+    },
+    notify: false
+  });
+});
+
+gulp.task('watch', ["browser-sync", "less"], function () {
+  gulp.watch("less/**/*.less", ['less']);
+  gulp.watch("*.html", browserSync.reload);
+  gulp.watch("js/**/*.js", browserSync.reload);
 });
